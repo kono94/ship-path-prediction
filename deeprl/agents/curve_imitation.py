fix errors related to normalization and integrate circles

deeprl/agents/curve_imitation.py
@@ -23,7 +23,9 @@ def set_seed(seed):
     # most crucial (and hidden) to shuffle deterministically
     torch.use_deterministic_algorithms(True)
     random.seed(seed)
+    np.random.seed(seed)
     np.random.default_rng(seed)
+
 
 
 class CustomFeedForwardPolicy(sb3.common.policies.ActorCriticPolicy):
@@ -54,11 +56,13 @@ def sample_expert_demonstrations(sample_env):
             obs.append(transition[0])
             actions.append(transition[1])
             infos.append(transition[2])
-            done = transition[3]    
+            done = transition[3]
+            sample_env.render(True)
         obs.append(sample_env.final_obs)
         trajectory_list.append(
             Trajectory(np.array(obs), np.array(actions), np.array(infos), terminal=True)
         )
+        
     #with open("curve_expert_trajectory.pickle", "wb") as handle:
    #     pickle.dump(trajectory_list, handle)
 

deeprl/envs/curve/curve_base.py
@@ -71,9 +71,13 @@ class CurveBase(gym.Env):
 
         # Curves to be generated
         self.trajectories = {
+            # "circle": {
+            #      "starting_pos": (350, 100),
+            #      "func": lambda t: 0.15 * t,
+            # },
             "sinus-small": {
-                "starting_pos": (20, 400),
-                "func": lambda t: math.sin(t / 1.5) - math.pi / 6,
+                 "starting_pos": (20, 400),
+                 "func": lambda t: math.sin(t / 1.5) - math.pi / 6,
             },
             "sinus-big": {
                 "starting_pos": (400, 20),
@@ -84,6 +88,19 @@ class CurveBase(gym.Env):
                 "func": lambda t: math.sin(t / 4) + math.pi / 6,
             },
         }
+        
+        for i in range(0):
+            x = random.randint(18, 35)
+            y = random.randint(8, 12)
+            a = np.random.uniform(3.7, 4.3)
+            b = np.random.uniform(5.7, 6.3)
+            c = np.random.uniform(0.37, 0.43)
+            start = random.randint(14, 16)
+            finish = start + random.randint(15, 19)
+            self.trajectories[f'circle{i}'] = {
+                "starting_pos": (x, y),
+                "func": lambda t:math.sin(t / a) + math.pi / b if t < start or t > finish else c * t,
+            }
         self.current_generator_curve = None
         self.current_generator_name = None
 

deeprl/envs/curve/curve_with_heading_speed.py
@@ -18,7 +18,7 @@ class CurveWithHeadingAndSpeed(CurveBase):
         super().__init__(animation_delay=animation_delay)
         self.action_space = spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
         self.observation_space = spaces.Box(
-            low=-1, high=1, shape=(3,), dtype=np.float32
+            low=-1, high=1, shape=(4,), dtype=np.float32
         )
         
     def _step_observation(self):
@@ -27,14 +27,17 @@ class CurveWithHeadingAndSpeed(CurveBase):
     def _expert_output(self, last_pos, last_heading, last_speed, expert_heading, expert_speed):
         return self._normalize_state([last_pos.x, last_pos.y, last_heading, last_speed]), \
                self._normalize_action([expert_heading, expert_speed])
-      
+    
+    def _next_agent_speed(self):
+        return self.current_action[1]
+    
     def reset(self):
         super()._reset()
-        return self._normalize_state([self.last_position.x, self.last_position.y, self.last_heading, self.last_speed])
+        return self._normalize_state([self.agent_position.x, self.agent_position.y, self.heading, self.speed])
     
     def reset_deterministically(self, idx):
         super()._reset_deterministically(idx)
-        return self._normalize_state([self.last_position.x, self.last_position.y, self.last_heading, self.last_speed])
+        return self._normalize_state([self.agent_position.x, self.agent_position.y, self.heading, self.speed])
 
 
 register(